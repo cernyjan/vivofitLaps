f09f7e269c13cf48c0008e2d61554e114cf69417
cernyjan <cerny.jan@hotmail.com>
2020-09-22 19:43:17 +0200

    fixed bug: "Sum of distance of laps != total distance" + added Activity type

--- vivofitLaps.py
+++ vivofitLaps.py
@@ -33,7 +33,7 @@ def seconds_into_time(seconds):
     return "{:.0f}:{:02.0f}:{:02.0f}".format(hour, minutes, seconds)
 
 def meters_into_kilometers(meters): 
-    return math.floor((meters / 1000.0) * 10 ** 2)  / 10 ** 2
+    return math.floor((meters / 1000.0) * 10 ** 3)  / 10 ** 3
 
 
 class Lap:
@@ -70,7 +70,10 @@ class Activity:
         return (finish_datetime - start_datetime)
 
     def get_rest_distance(self, distance_meters):
-        return math.floor(((self.total_distance_meters / 1000.0) - distance_meters) * 10 ** 2)  / 10 ** 2
+        return math.floor(((self.total_distance_meters / 1000.0) - distance_meters) * 10 ** 3)  / 10 ** 3
+
+    def get_rest_distance_approx(self, prev_laps_count):
+        return math.floor(((self.total_distance_meters / 1000.0) - prev_laps_count) * 10 ** 3)  / 10 ** 3
 
     def set_laps(self):
         lap_number = 1
@@ -82,20 +85,21 @@ class Activity:
             else:
                 start_datetime = get_date_format(activity.id)
                 distance_meters = 0.0
-                finish_distance = distance_meters
+                #finish_distance = distance_meters
                 trackpoint_number = 1
                 trackpoint_count = len(self.track)
                 for trackpoint in self.track:
                     distance_meters = into_float(trackpoint[1].text)
                     if distance_meters // 1000 >= lap_number:
                         finish_datetime = get_date_format(trackpoint[0].text)
-                        finish_distance = distance_meters / 1000.0
+                        #finish_distance = distance_meters / 1000.0
                         self.laps.append(Lap("{}.".format(lap_number), self.get_lap_time(start_datetime, finish_datetime)))
                         start_datetime = finish_datetime
                         lap_number = lap_number + 1
                     elif trackpoint_number == trackpoint_count:
                         finish_datetime = get_date_format(trackpoint[0].text)
-                        self.laps.append(Lap("{}.".format(lap_number), self.get_lap_time(start_datetime, finish_datetime), self.get_rest_distance(finish_distance)))
+                        #self.laps.append(Lap("{}.".format(lap_number), self.get_lap_time(start_datetime, finish_datetime), self.get_rest_distance(finish_distance)))
+                        self.laps.append(Lap("{}.".format(lap_number), self.get_lap_time(start_datetime, finish_datetime), self.get_rest_distance_approx(lap_number - 1)))
                     trackpoint_number = trackpoint_number + 1
 
     def render_into_table(self):
@@ -103,19 +107,19 @@ class Activity:
         laps = []
         times = []
         distances = []
-        laps.append("{} v{}".format(self.device_name, self.device_version))
+        laps.append("{} v{} ({})".format(self.device_name, self.device_version, self.activity_type))
         times.append("{} (total)".format(seconds_into_time(self.total_time_seconds)))
-        distances.append("{} (total)".format(meters_into_kilometers(self.total_distance_meters)))
+        distances.append("{:.03f} (total)".format(meters_into_kilometers(self.total_distance_meters)))
         for lap in self.laps:
             laps.append(lap.id)
             times.append(lap.time)
-            distances.append("{:.02f}".format(lap.distance))        
+            distances.append("{:.03f}".format(lap.distance))        
 
         titles = ['Laps', 'Times [h:m:s]', 'Distances [km]']
         data = [titles] + list(zip(laps, times, distances))
 
         for i, d in enumerate(data):
-            line = '|'.join(str(x).ljust(20) for x in d)
+            line = '|'.join(str(x).ljust(30) for x in d)
             print(line)
             if i == 0:
                 print('-' * len(line))
